40fa1abd473d73c68cf8037af70fe5ed1601c71b
NoobLk <125950957+nooblk-98@users.noreply.github.com>
2026-03-13 09:26:53 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,26 @@ set -eu
 
 cd /var/www/html
 
+APP_USER="1000"
+APP_GROUP="1000"
+
 mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views storage/logs bootstrap/cache || true
-chown -R application:application storage bootstrap/cache || true
-chmod -R ug+rwx storage bootstrap/cache || true
-
-
-if [ -f artisan ] && [ -f vendor/autoload.php ]; then
-	if ! grep -q '^APP_KEY=base64:' .env 2>/dev/null; then
-		php artisan key:generate || true
-	fi
-
-	php artisan storage:link --force || true
-	php artisan optimize|| true
+TODAY_LOG="storage/logs/laravel-$(date +%F).log"
+if [ ! -f "$TODAY_LOG" ]; then
+	touch "$TODAY_LOG" || true
 fi
+
+chown -R "$APP_USER:$APP_GROUP" storage bootstrap/cache || true
+chmod -R ug+rwx storage bootstrap/cache || true
+chown "$APP_USER:$APP_GROUP" "$TODAY_LOG" || true
+chmod 664 "$TODAY_LOG" || true
+
+
+php artisan config:clear && php artisan config:cache || true
+php artisan route:clear || true
+php artisan view:clear && php artisan view:cache || true
+php artisan storage:link --force || true
+
+# if [ -f /var/www/html/pm2_cronjobs/start-pm2-jobs.sh ]; then
+# 	sh /var/www/html/pm2_cronjobs/start-pm2-jobs.sh || true
+# fi
